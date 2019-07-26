dd618ba00ea0c62569bbcc7df18aa188a3d2ed60
johnnylord <johnnylord7410@gmail.com>
2019-07-26 10:45:21 +0800

    Implement generic streaming class

---
+++ stream.py
@@ -0,0 +1,87 @@
+import time
+from queue import Queue
+from threading import Thread
+import cv2
+
+class VideoStream:
+
+    def __init__(self, src=0, transform=None, queue_size=256):
+        """Initialize the video stream
+
+        [Arguments]
+        - src
+            The source of the video. It can be a integer, used as index of
+            the webcam on your system. It can be a string, used as either
+            the filename or the url of the ip camera.
+        - transform
+            Helper function to transform the frame received from the stream
+        - queue_size
+            The size of the stream frame buffer
+        """
+        self.stream = cv2.VideoCapture(src)
+        self.stopped = False
+        self.transform = transform
+        self.frame_queue = Queue(maxsize=queue_size)
+        self.thread = Thread(target=self._update, args=())
+        self.thread.daemon = True
+
+    def start(self):
+        """Start streaming"""
+        self.thread.start()
+        return self
+
+    def stop(self):
+        """Stop streaming and release resource"""
+        self.stopped = True
+        self.thread.join()
+
+    def read(self):
+        """Return next frame"""
+        return self.frame_queue.get()
+
+    def size(self):
+        """Return number of frames in the queue_buffer"""
+        return self.frame_queue.qsize()
+
+    def _update(self):
+        """Thread to fill the stream frame buffer"""
+        while True:
+            # stop the thread
+            if self.stopped:
+                break
+
+            # add new frame if there is room for it in frame_queue
+            if not self.frame_queue.full():
+                ret, frame = self.stream.read()
+
+                # reach the end of the video stream, stop the thread
+                if not ret:
+                    self.stopped = True
+                    continue
+
+                # apply transformation
+                if self.transform:
+                    frame = self.transform(frame)
+
+                self.frame_queue.put(frame)
+            else:
+                # wait 10ms for consumer consuming the frames
+                time.sleep(0.1)
+
+        self.stream.release()
+
+
+if __name__ == "__main__":
+    vs = VideoStream()
+
+    vs.start()
+
+    while vs.size() or not vs.stopped:
+        frame = vs.read()
+        cv2.imshow("Test", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord('q'):
+            vs.stop()
+
+    cv2.destroyAllWindows()
